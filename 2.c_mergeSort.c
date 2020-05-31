3c46e927b00375ee3e4bcd10366554c6c0a94046
yashmudgal05 <55059719+yashmudgal05@users.noreply.github.com>
2020-05-31 15:52:34 +0530

    Create 2.c_mergeSort.c

---
+++ 2.c_mergeSort.c
@@ -0,0 +1,72 @@
+#include<stdio.h>
+
+void merge(int A[], int l, int mid, int h)
+{
+    int i=l,j=mid+1,k=l;
+
+    int B[100];
+
+    while(i<=mid && j<=h)
+    {
+        if(A[i] < A[j])
+        {
+            B[k++] = A[i++];
+        }
+        else
+        {
+            B[k++] = A[j++];
+        }
+    }
+
+    for( ; i<=mid; i++)
+    {
+        B[k++] = A[i];
+    }
+    for( ; j<=h; j++)
+    {
+        B[k++] = A[j];
+    }
+
+    for(i=l; i<=h; i++)
+    {
+        A[i] = B[i];
+    }
+}
+
+void recursiveMergeSort(int A[], int l, int h)
+{
+    int mid;
+
+    if(l < h)
+    {
+        mid = (l+h)/2;
+
+        recursiveMergeSort(A,l,mid);
+        recursiveMergeSort(A,mid+1,h);
+        merge(A,l,mid,h);
+    }
+}
+
+void display(int A[], int n)
+{
+    int i;
+
+    for(i=0;i<n;i++)
+    {
+        printf("%d\t",A[i]);
+    }
+    printf("\n");
+}
+
+int main()
+{
+    int A[] = {11,13,7,12,16,9,24,5,10,3};
+    int n = 10;
+
+    recursiveMergeSort(A,0,9);
+
+    display(A,n);
+
+    return 0;
+}
+
